16b18ca18788179ad93d2d46417488249811b476
haidour <ga_haidour@esi.dz>
2021-01-02 14:36:23 +0100

    Multiple  regression compléte

---
+++ MultipleRegression.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  2 14:21:31 2021
+
+@author: Mac
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#importer le dataset
+dataset= pd.read_csv('50_Startups.csv')
+#Extraire les variables indépendantes 
+
+#Extraire la variables dépendante
+
+#Afficher  le graphe 
