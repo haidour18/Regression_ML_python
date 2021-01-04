ce086be95676352ea624f57116a1730379dcc660
haidour <ga_haidour@esi.dz>
2021-01-04 02:20:17 +0100

    multiple regression

--- MultipleRegression.py
+++ MultipleRegression.py
@@ -37,3 +37,33 @@ print('intercept:a', regressor.intercept_)
 print('slope:b', regressor.coef_)
 
 y_pred = regressor.predict(X_test)
+
+#Building a model using backward elimination 
+import statsmodels.api as sm
+#add b1X1 to oour model with x1=1 Step 1
+X = np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1)
+#Step  fit the full model with all possible predictors
+X_opt = X[:,[0,1,2,3,4,5]]
+X_opt = np.array(X_opt, dtype=float) 
+regressor_OLS = sm.OLS(endog =y ,exog =X_opt).fit()
+print(regressor_OLS.summary())
+X_opt = X[:,[0,1,3,4,5]]
+X_opt = np.array(X_opt, dtype=float) 
+regressor_OLS = sm.OLS(endog =y ,exog =X_opt).fit()
+print(regressor_OLS.summary())
+
+X_opt = X[:,[0,3,4,5]]
+X_opt = np.array(X_opt, dtype=float) 
+regressor_OLS = sm.OLS(endog =y ,exog =X_opt).fit()
+print(regressor_OLS.summary())
+
+
+X_opt = X[:,[0,3,5]]
+X_opt = np.array(X_opt, dtype=float) 
+regressor_OLS = sm.OLS(endog =y ,exog =X_opt).fit()
+print(regressor_OLS.summary())
+
+X_opt = X[:,[0,3]]
+X_opt = np.array(X_opt, dtype=float) 
+regressor_OLS = sm.OLS(endog =y ,exog =X_opt).fit()
+print(regressor_OLS.summary())
